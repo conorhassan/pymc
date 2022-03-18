a659e56c9ac3b85d51f039c2364b54289a99be18
Ravin Kumar <ravinsdrive@gmail.com>
2022-03-18 07:07:49 -0700

    Update pymc/tests/test_distributions.py
    Co-authored-by: Ricardo Vieira <28983449+ricardoV94@users.noreply.github.com>

--- pymc/tests/test_distributions.py
+++ pymc/tests/test_distributions.py
@@ -2620,7 +2620,7 @@ class TestMatchesScipy:
     def test_grw(self):
         self.check_logp(
             pm.GaussianRandomWalk,
-            R,
+            Vector(R, 10)
             {"mu": R, "sigma": Rplus, "steps": Nat},
             lambda value, mu, sigma: sp.norm.logpdf(value, mu, sigma).cumsum().sum(),
             decimal=select_by_precision(float64=6, float32=1),
